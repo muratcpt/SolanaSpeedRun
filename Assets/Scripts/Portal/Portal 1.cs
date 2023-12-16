00a2e89aa33c6d97dd7cdecd9b509879f45f1b32
muratcpt <muratcpt332@gmail.com>
2023-12-16 20:13:32 +0300

    portals

---
+++ Assets/Scripts/Portal/Portal 1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Portal1 : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject.name == "MainChar")
+        {
+            Invoke("Portal", 2f);
+            
+        }
+        
+    }
+
+    private void Portal()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
