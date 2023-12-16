b58fef78ef347e775be10674f32ab6b0709a27e5
muratcpt <muratcpt332@gmail.com>
2023-12-16 22:03:17 +0300

    changes

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
+    private Animator anim;
+    private bool isPressing;    
 
-    private float bounce = 20f;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private float bounce = 10f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            anim.SetBool("press", true);
+            isPressing = true;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+    }
+    private void Update()
+    {
+        if (isPressing && !anim.GetCurrentAnimatorStateInfo(0).IsName("press")) 
+        {
+            anim.SetBool("press", false);
+            isPressing = false;
         }
     }
 
